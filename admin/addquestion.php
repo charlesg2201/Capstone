6a530726d53b8dcc56df69f2c15e3013df087801
charlesg2201 <mnznchrls@gmail.com>
2023-12-01 13:23:13 +0800

    choices edited

--- admin/addquestion.php
+++ admin/addquestion.php
@@ -5,103 +5,101 @@ include('header.php');
 include('sidebar.php');
 include('connect.php');
 
+// Check if the edit button is clicked
+if (isset($_GET['editid'])) {
+    $editId = $_GET['editid'];
+
+    // Retrieve the existing choices for the selected question ID
+    $selectQuery = "SELECT questions, question_type, choices FROM tbl_physical WHERE question_id = $editId";
+    $result = mysqli_query($conn, $selectQuery);
+
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        $existingQuestions = $row['questions'];
+        $existingQuestionType = $row['question_type'];
+        $existingChoices = $row['choices'];
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+
+// Rest of your existing PHP code...
+
 if (isset($_POST['btn_submit'])) {
     $questions = $_POST['questions'];
     $question_type = $_POST['question_type'];
-    $choices = $_POST['choices'];
 
-    if(isset($_GET['editid']))
-            {
-                $sql ="UPDATE tbl_physical SET questions='$_POST[questions]', question_type='$_POST[question_type]', choices='$_POST[choices]' WHERE question_id='$_GET[editid]'";
-                if($qsql = mysqli_query($conn,$sql))
-                {
-        ?>
-                    <div class="popup popup--icon -success js_success-popup popup--visible">
-                    <div class="popup__background"></div>
-                    <div class="popup__content">
-                        <h3 class="popup__content__title">
-                        Success
-                        </h3>
-                        <p>Updated Successfully</p>
-                        <p>
-                        <!--  <a href="index.php"><button class="button button--success" data-for="js_success-popup"></button></a> -->
-                        <?php echo "<script>setTimeout(\"location.href = 'manage_physical.php';\",1500);</script>"; ?>
-                        </p>
-                    </div>
-                    </div>
-        <?php
-                }
-                else
-                {
-                    echo mysqli_error($conn);
-                }
-            
-           
-            } else {
-                $sql = "INSERT INTO tbl_physical (questions, question_type, choices) VALUES (?, ?, ?)";
-                $stmt = mysqli_prepare($conn, $sql);
-
-                if ($stmt) {
-                    mysqli_stmt_bind_param($stmt, "sss", $questions, $question_type, $choices);
-                    $result = mysqli_stmt_execute($stmt);
-
-                    if ($result) {
-        ?>
-                        <div class="popup popup--icon -success js_success-popup popup--visible">
-                            <div class="popup__background"></div>
-                            <div class="popup__content">
-                                <h3 class="popup__content__title">Success</h3>
-                                <p>Record Inserted Successfully</p>
-                                <?php echo "<script>setTimeout(\"location.href = 'manage_physical.php';\",1500);</script>"; ?>
-                            </div>
-                        </div>
-        <?php
-                    } else {
-                        echo mysqli_error($conn);
-                    }
-
-                    mysqli_stmt_close($stmt);
-                } else {
-                    echo mysqli_error($conn);
-                }
-            }
-        }
-
-if(isset($_GET['editid']))
-    {
-        $sql="SELECT * FROM tbl_physical WHERE question_id='$_GET[editid]' ";
-        $qsql = mysqli_query($conn,$sql);
-        $rsedit = mysqli_fetch_array($qsql);
-
+    if (isset($_POST['choices']) && is_array($_POST['choices'])) {
+        $choices = implode(', ', $_POST['choices']);
+    } else {
+        $choices = '';
     }
 
+    if (isset($_GET['editid'])) {
+        $sql = "UPDATE tbl_physical SET questions='$questions', question_type='$question_type', choices='$choices' WHERE question_id='$editId'";
+        if ($qsql = mysqli_query($conn, $sql)) {
+?>
+            <div class="popup popup--icon -success js_success-popup popup--visible">
+                <div class="popup__background"></div>
+                <div class="popup__content">
+                    <h3 class="popup__content__title">Success</h3>
+                    <p>Updated Successfully</p>
+                    <?php echo "<script>setTimeout(\"location.href = 'manage_physical.php';\",1500);</script>"; ?>
+                </div>
+            </div>
+<?php
+        } else {
+            echo mysqli_error($conn);
+        }
+    } else {
+        $sql = "INSERT INTO tbl_physical (questions, question_type, choices) VALUES (?, ?, ?)";
+        $stmt = mysqli_prepare($conn, $sql);
+
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "sss", $questions, $question_type, $choices);
+            $result = mysqli_stmt_execute($stmt);
+
+            if ($result) {
+?>
+                <div class="popup popup--icon -success js_success-popup popup--visible">
+                    <div class="popup__background"></div>
+                    <div class="popup__content">
+                        <h3 class="popup__content__title">Success</h3>
+                        <p>Record Inserted Successfully</p>
+                        <?php echo "<script>setTimeout(\"location.href = 'manage_physical.php';\",1500);</script>"; ?>
+                    </div>
+                </div>
+<?php
+            } else {
+                echo mysqli_error($conn);
+            }
+
+            mysqli_stmt_close($stmt);
+        } else {
+            echo mysqli_error($conn);
+        }
+    }
+}
+
+// Rest of your existing PHP code...
 
 ?>
 
-    
-    <script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>
+<script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>
 
-    <div class="pcoded-content">
+<div class="pcoded-content">
     <div class="pcoded-inner-content">
         <div class="main-body">
             <div class="page-body">
                 <div class="card">
                     <div class="card-header"><h1>Physical Assessment</h1></div>
                     <div class="card-block">
-        <hr>
-    <form id="main" method="post" action="" enctype="multipart/form-data">
-    <style>
-                                #Question {
-                                    height: 100px; /* Adjust the height as needed */
-                                }
-                            </style>
-
+                        <hr>
+                        <form id="main" method="post" action="" enctype="multipart/form-data">
                             <div class="form-group row">
                                 <label class="col-sm-2 col-form-label">Question</label>
                                 <div class="col-sm-10">
-                                    <textarea class="form-control" name="questions" id="questions" placeholder="Enter a Question" required=""><?php if (isset($_GET['editid'])) {
-                                                                                                                                                    echo $rsedit['questions'];
-                                                                                                                                                } ?></textarea>
+                                    <textarea class="form-control" name="questions" id="questions" placeholder="Enter a Question" required=""><?php echo isset($existingQuestions) ? $existingQuestions : ''; ?></textarea>
                                     <span class="messages"></span>
                                 </div>
                             </div>
@@ -113,7 +111,7 @@ if(isset($_GET['editid']))
                                         <?php
                                         $arr = array("Multiple Choice", "Essay");
                                         foreach ($arr as $val) {
-                                            if ($val == $rsedit['question_type']) {
+                                            if ($val == $existingQuestionType) {
                                                 echo "<option value='$val' selected>$val</option>";
                                             } else {
                                                 echo "<option value='$val'>$val</option>";
@@ -124,56 +122,72 @@ if(isset($_GET['editid']))
                                     <span class="messages"></span>
                                 </div>
                             </div>
-                            <div class="form-group row" id="choicesContainer" style="display: none;">
-                                <label class="col-sm-2 col-form-label">Choices</label>
-                                <div class="col-sm-4">
-                                    <textarea class="form-control" name="choices" id="choices"></textarea>
-                                    <span class="messages"></span>
+                            <div class="form-group row" id="choicesContainer" style="display: <?php echo (isset($existingQuestionType) && $existingQuestionType == 'Multiple Choice') ? 'block' : 'none'; ?>;">
+    <label class="col-sm-2 col-form-label">Choices</label>
+    <div class="col-sm-4" id="dynamicChoices">
+        <?php
+        if (isset($existingQuestionType) && $existingQuestionType == 'Multiple Choice') {
+            $choicesArray = explode(', ', $existingChoices);
+            foreach ($choicesArray as $index => $choice) {
+                echo '<textarea class="form-control" name="choices[]" placeholder="Enter Choice ' . ($index + 1) . '">' . $choice . '</textarea>';
+            }
+        } else {
+            // Display nothing or customize as needed when question type is not "Multiple Choice"
+            // For example, you might display a message or leave it empty
+            echo '<textarea style="display: none;" class="form-control" name="choices[]" placeholder="Enter Choice 1"></textarea>';
+        }
+        ?>
+    </div>
+</div>
+
+
+                            <div class="form-group row">
+                                <label class="col-sm-2"></label>
+                                <div class="col-sm-10">
+                                    <button type="submit" name="btn_submit" class="btn btn-primary m-b-0">Submit</button>
                                 </div>
                             </div>
-                                 
-
-
-
-        <div class="form-group row">
-            
-        </div>
-        
-
-        <div class="form-group row">
-            
-        </div>
-        
-    <?php
-     ?>
-
-        <div class="form-group row">
-            <label class="col-sm-2"></label>
-            <div class="col-sm-10">
-                <button type="submit" name="btn_submit" class="btn btn-primary m-b-0">Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+</div>
 
-    </form>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    <script>
-   document.getElementById('question_type').addEventListener('change', function() {
-    var choicesContainer = document.getElementById('choicesContainer');
-    var selectedType = this.value;
-    if (selectedType === 'Multiple Choice') {
-        choicesContainer.style.display = 'block';
-    } else {
-        choicesContainer.style.display = 'none';
-    }
-});
+<script>
+    document.getElementById('question_type').addEventListener('change', function () {
+        var choicesContainer = document.getElementById('choicesContainer');
+        var dynamicChoicesContainer = document.getElementById('dynamicChoices');
+        var selectedType = this.value;
 
+        // Clear existing choices when changing the question type
+        dynamicChoicesContainer.innerHTML = '';
+
+        if (selectedType === 'Multiple Choice') {
+            var numChoices = prompt("Enter the number of choices:");
+
+            if (numChoices) {
+                for (var i = 1; i <= numChoices; i++) {
+                    var textarea = document.createElement('textarea');
+                    textarea.className = 'form-control';
+                    textarea.name = 'choices[]'; // Use an array to handle multiple choices
+                    textarea.placeholder = 'Enter Choice ' + i;
+                    dynamicChoicesContainer.appendChild(textarea);
+                }
+
+                choicesContainer.style.display = 'block';
+            } else {
+                this.value = '';
+                choicesContainer.style.display = 'none';
+            }
+        } else {
+            choicesContainer.style.display = 'none';
+        }
+    });
 </script>
-    <?php include('footer.php');?>
+
+
+<?php
+include('footer.php');
+?>
--- admin/editquestions.php
+++ admin/editquestions.php
@@ -7,15 +7,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if checkboxes are selected
     if (isset($_POST['record']) && is_array($_POST['record'])) {
         foreach ($_POST['record'] as $selectedId) {
-            // Perform an update query for each selected item
-            $updateQuery = "UPDATE tbl_physical SET column_to_update = 'new_value' WHERE question_id = $selectedId";
-            mysqli_query($conn, $updateQuery);
-        }
+            // Retrieve the existing choices for the selected question ID
+            $selectQuery = "SELECT choices FROM tbl_physical WHERE question_id = $selectedId";
+            $result = mysqli_query($conn, $selectQuery);
 
-        // Redirect or display a success message after updating
-        header("Location: your_success_page.php");
-        exit();
-    } 
+            if ($result) {
+                $row = mysqli_fetch_assoc($result);
+                $existingChoices = $row['choices'];
+
+                // Now you can use $existingChoices to display or modify the choices in your form
+            } else {
+                echo "Error: " . mysqli_error($conn);
+            }
+        }
+    } else {
+        echo "No records selected.";
+    }
 } else {
     // If the form is not submitted via POST, handle it accordingly
     echo "Invalid request.";
