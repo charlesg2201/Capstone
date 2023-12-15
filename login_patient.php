b39da3008c6ab6c9773a228c047027a3b0f3a31a
paulmoji <138263496+paulmoji@users.noreply.github.com>
2023-12-15 19:16:56 +0800

    responsive login_patient.php

--- login_patient.php
+++ login_patient.php
@@ -2,8 +2,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" href="popup_style.css">
-
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <head>
 <title>Patient</title>
 
