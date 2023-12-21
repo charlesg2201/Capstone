dbaf43bfa60ad9104e68f14954f5ba53cb2e04b9
charlesg2201 <mnznchrls@gmail.com>
2023-12-21 21:19:13 +0800

    update message fetch id

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $row_manage = mysqli_fetch_array($result_manage);
                                 <div class="card text-white mb-8" style="background-color: #135e96;">
                                     <div class="card-body" style="font-size: 25px; font-weight: bold;">Messages</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between" style="background-color: #0a4b78;">
-                                    <a class="small stretched-link" href="messages.php" style="color: #ffffff; font-size: 12px;">Inbox</a>
+                                    <a class="small stretched-link" href="messages.php?patientid=<?php echo $_SESSION["patientid"]; ?>" style="color: #ffffff; font-size: 12px;">Inbox</a>
                                         <div class="small text-white"><i class="feather icon-mail" style="font-size: 24px;"></i></div>
                                     </div>
                                 </div>
