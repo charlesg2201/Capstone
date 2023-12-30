8b6d6e20d3fd130545d50510f28f35019a1bebd6
paulmoji <mojicapaul015@gmail.com>
2023-12-30 09:19:33 +0800

    message ui

--- admin/messages.php
+++ admin/messages.php
@@ -8,7 +8,34 @@
     include('sidebar.php');
     include('connect.php');
     ?>
-    
+      <div class="pcoded-content">
+        <div class="pcoded-inner-content">
+            <div class="main-body">
+            <div class="page-wrapper">
+
+<div class="page-header">
+<div class="row align-items-end">
+<div class="col-lg-8">
+<div class="page-header-title">
+<div class="d-inline">
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="page-header-breadcrumb">
+<ul class="breadcrumb-title">
+<li class="breadcrumb-item">
+ <i class="feather icon-home"></i>
+</li>
+<li class="breadcrumb-item"><a href="index.php">Home</a>
+</li>
+<li class="breadcrumb-item"><a href="messages.php">Message</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
     <style>
         body, html {
             height: 100%;
